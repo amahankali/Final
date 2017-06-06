1bc801eacf21d5897f13935544f3eb84e0c0cb05
arvind <amahankali@stuy.edu>
2017-06-06 15:03:56 -0400

    plot with zbuffer

--- display.c
+++ display.c
@@ -36,7 +36,13 @@ jdyrlandweaver
 void plot( screen s, zbuffer zb, color c, int x, int y, double z) {
   int newy = YRES - 1 - y;
   if ( x >= 0 && x < XRES && newy >=0 && newy < YRES )
-    s[x][newy] = c;
+  {
+    if(zb[x][newy] <= z)
+    {
+      zb[x][newy] = z;
+      s[x][newy] = c;
+    }
+  }
 }
 
 /*======== void clear_screen() ==========
